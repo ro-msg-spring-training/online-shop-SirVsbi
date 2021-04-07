dc0e1edc63ea7cd058e00f270a7de40d6ab77d2f
vidams <Szabolcs.Vidam@msg.group>
2021-04-07 16:08:47 +0300

    Add initial flyway migration files.

---
+++ src/main/resources/db/migration/V1_1__ExtendedSchemaProduct.sql
@@ -0,0 +1,30 @@
+CREATE TABLE IF NOT EXISTS `Supplier`
+(
+    SupplierId IDENTITY NOT NULL PRIMARY KEY,
+    Name       VARCHAR(255)
+);
+
+CREATE TABLE IF NOT EXISTS `ProductCategory`
+(
+    ProductCategoryId IDENTITY NOT NULL PRIMARY KEY,
+    Name              VARCHAR(255),
+    Description       VARCHAR(255)
+);
+
+CREATE TABLE IF NOT EXISTS `Product`
+(
+    ProductId         IDENTITY NOT NULL PRIMARY KEY,
+    Name              VARCHAR(255),
+    Description       VARCHAR(255),
+    Price             DECIMAL, //BigDecimal(Java) -> Decimal(sql/H2)
+    Weight            Double,
+    ProductCategoryId BIGINT   NOT NULL,
+    SupplierId        BIGINT   NOT NULL,
+    ImageUrl          VARCHAR(255),
+    CONSTRAINT FK_PRODUCT_CATEGORY FOREIGN KEY (ProductCategoryId) references ProductCategory (ProductCategoryId),
+    CONSTRAINT FK_PRODUCT_SUPPLIER FOREIGN KEY (SupplierId) references Supplier (SupplierId)
+)
+
+
+
+
